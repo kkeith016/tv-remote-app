c22f9dba429d2b87e09c0bcb925bb2793eb1fddf
Kylee <kkeith.biz@gmail.com>
2025-10-01 15:50:37 -0400

    overloaded method

--- src/main/java/com/pluralsight/TVRemote.java
+++ src/main/java/com/pluralsight/TVRemote.java
@@ -8,6 +8,49 @@ public class TVRemote {
     private int currentVolume;
     private boolean powerOn;
 
+    //Constructor
+    public TVRemote(String manufacturerName){
+        this.manufacturer = manufacturerName;
+        this.currentChannel = 1; //Starting channel
+        this.currentVolume = 10; //starting volume
+        this.powerOn = false; //TV starts on off
+    }
+
+    //overloaded Constructor specifying manufacture starting channel and starting volume
+     TVRemote(String manufacturerName, int startChannel, int startVolume) {
+         this.manufacturer = manufacturerName;
+         this.currentChannel = startChannel;
+         this.currentVolume = startVolume;
+         this.powerOn = false;
+     }
+
+    //Methods
+    //Power on and off.
+
+    public void turnOn(){
+        this.powerOn = true;
+        System.out.println(manufacturer + " Tv is now ON");
+    }
+
+    public void turnOff(){
+        this.powerOn = false;
+        System.out.println(manufacturer + " Tv is now OFF");
+    }
+
+    //Changing the channel
+    public void changeChannel(int newChannel){
+        this.currentChannel = newChannel;
+        System.out.println("Tv channel changed to " + newChannel);
+    }
+
+    //Volume
+    public void adjustVolume(int newVolume){
+        currentVolume = newVolume;
+        System.out.println("Tv volume adjusted to " + newVolume);
+    }
+
+
+
     public String getManufacturer() {
         return manufacturer;
     }
@@ -40,12 +83,5 @@ public class TVRemote {
         this.powerOn = powerOn;
     }
 
-    //Constructor
-    public TVRemote(String manufacturerName){
-        this.manufacturer = manufacturerName;
-        this.currentChannel = 1; //Starting channel
-        this.currentVolume = 10; //starting volume
-        this.powerOn = false; //TV starts on off
-    }
 
 }
